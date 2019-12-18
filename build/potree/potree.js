3b40278d8abab4d8dbcb21d378febdd17b527356
hudsonchase <hudsonchase410@gmail.com>
2019-12-18 12:02:57 -0500

    update

--- build/potree/potree.js
+++ build/potree/potree.js
@@ -12729,7 +12729,7 @@ Potree.Viewer = class PotreeViewer extends THREE.EventDispatcher{
 
 		this.progressBar = new ProgressBar();
 
-		//this.stats = new Stats();
+		this.stats = new Stats();
 		//this.stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
 		//document.body.appendChild( this.stats.dom );
 		//this.stats.dom.style.left = "100px";
