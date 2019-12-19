7a318826a0030f4572664a59c1e781426e173861
hudsonchase <hudsonchase410@gmail.com>
2019-12-19 14:26:11 -0500

    test

---
+++ jsm/pmrem/PMREMGenerator.d.ts
@@ -0,0 +1,19 @@
+import {
+	WebGLRenderer,
+	WebGLRenderTarget,
+	Texture,
+	CubeTexture,
+	Scene
+} from '../../../src/Three';
+
+export class PMREMGenerator {
+
+	constructor( renderer:WebGLRenderer );
+	fromScene( scene:Scene, sigma?:number, near?:number, far?:number ): WebGLRenderTarget;
+	fromEquirectangular( equirectangular:Texture ): WebGLRenderTarget;
+	fromCubemap( cubemap:CubeTexture ): WebGLRenderTarget;
+	compileCubemapShader(): void;
+	compileEquirectangularShader(): void;
+	dispose(): void;
+
+}
